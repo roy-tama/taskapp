d0e555a3f23e3a75e28b79fbafef290936e3a4a9
tatsu.maki <tatsuya.spring0414@gmail.com>
2023-01-01 19:09:34 +0900

    taskクラス追加

---
+++ taskapp/Task.swift
@@ -0,0 +1,27 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 牧野達也 on 2023/01/01.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // 日時
+    @objc dynamic var date = Date()
+    
+    // IDをプライマリキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
