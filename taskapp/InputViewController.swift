9b8b18cb1ef561ec20f543750cc3eaa6bce70c19
tatsu.maki <tatsuya.spring0414@gmail.com>
2023-01-02 12:04:23 +0900

    制御追加

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -6,28 +6,44 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var dataPicker: UIDatePicker!
+    var task: Task!
+    let realm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶ
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        // 各プロパティにtaskオブジェクトの対応する内容を代入
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        dataPicker.date = task.date
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
     }
-    */
-
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.dataPicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
 }
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Realmインスタンスの取得
+    let realm = try!Realm()
+    
+    // DB内のタスクが格納されるリスト
+    // 日付の近い順でソート
+    // 以降、内容をアップデートするとリスト内は自動で更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -22,14 +31,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // セルの数を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
     
     // 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能なセルを取得
-        let Cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        return Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString: String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+
+        return cell
     }
     // 各セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -42,7 +60,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     // Deleteボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            // データベースから削除する
+            try! realm.write{
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let inputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            inputViewController.task = task
+        }
+    }
+    // 入力画面から戻ってきた時にTableViewを更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 }
 
